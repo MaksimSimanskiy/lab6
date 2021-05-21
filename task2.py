4d2c75f8e8ded5a6061e45910f9d372650e43729
MaksimSimanskiy <maks.simanskiy@gmail.com>
2021-05-21 09:01:56 +0300

    final

--- task2.py
+++ task2.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-c = int(input("Введите начало интервала "))
-d = int(input("Введите конец интервала "))
-a = []
 
-for i in range(5):
-    a.append(float(input(f'Введите элемент списка {i} - ')))
+if __name__ == '__main__':
+    c = int(input("Введите начало интервала "))
+    d = int(input("Введите конец интервала "))
+    a = []
+    k = 0;
+    for i in range(5):
+        a.append(float(input(f'Введите элемент списка {i} - ')))
 
-print('Максимальный элемент ' , max(a, key=abs))
-print('Индекс максимального элемента ' , a.index(max(a, key=abs)))
-for i in a:
-    if (a[i] > 0.):
-        k = i
-        break
-print('Сумма чисел после первого положительного числа',sum(a[k:]))
-print('Отсортированый массив',sorted(a,key = lambda x: d > x > c,reverse = True))
+    print('Максимальный элемент ' , max(a, key=abs))
+    print('Индекс максимального элемента ' , a.index(max(a, key=abs)))
+
+    k =(list(filter(lambda i: i > 0, a))[0])
+    k = a.index(k)
+    print('Сумма чисел после первого положительного числа',sum(a[k:]))
+    print('Отсортированый массив',sorted(a,key = lambda x: d > x > c,reverse = True))

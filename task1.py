c2bc377587fae49521f723259dc03af28a28731f
MaksimSimanskiy <maks.simanskiy@gmail.com>
2021-05-21 09:05:07 +0300

    end

--- task1.py
+++ task1.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-a = []
-b = []
-for i in range(10):
-    a.append(int(input(f'Введите элемент списка {i} - ')))
-    b = list(filter(lambda x: 20 > x > 2 and x % 8 == 0, a))
-print('Список А = {}\nВыбраные элементы = {} \nКоличество элементов  = {} \nСумма = {}'.format(a, b, len(b), sum(b)))
+
+if __name__ == '__main__':
+    a = []
+    b = []
+    for i in range(10):
+        a.append(int(input(f'Введите элемент списка {i} - ')))
+        b = list(filter(lambda x: 20 > x > 2 and x % 8 == 0, a))
+    print('Список А = {}\nВыбраные элементы = {} \nКоличество элементов  = {} \nСумма = {}'.format(a, b, len(b), sum(b)))
